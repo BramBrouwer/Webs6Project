8c4792911ccf7f06b87fd046a4c7fe6fb620b351
bbrouwer <b.brouwer@student.avans.nl>
2016-06-06 11:34:49 +0200

    Update

--- app/js/GameController.js
+++ app/js/GameController.js
@@ -1,16 +1,28 @@
-module.exports = function($scope, $http,$q, GameFactory){
+module.exports = function ($scope, $http, $q, GameFactory) {
     var self = this;
     self.GameFactory = GameFactory;
-    
-    
-    
-     self.joinGame = function(game){
 
-                if(game.users.length < 2){
-                //   game.users[game.users.length] = "Bram";
-                self.GameFactory.joinGame(game);
-                }else{
-                    console.log("Game is full already");
-                }            
+
+
+    self.joinGame = function (game) {
+
+        if (game.users.length < 2) {
+            //   game.users[game.users.length] = "Bram";
+            self.GameFactory.joinGame(game);
+        } else {
+            console.log("Game is full already");
         }
+    }
+
+    self.getGames = function () {
+        $http({
+            method: 'GET',
+            url: 'https://mahjongmayhem.herokuapp.com/Games'
+        }).then(function successCallback(response) {
+            console.log(response.data);
+            self.GameFactory.getGames(response.data);
+        }, function errorCallback(response) {
+          console.log(response);
+        });
+    }
 }
--- app/js/GameFactory.js
+++ app/js/GameFactory.js
@@ -7,13 +7,22 @@ module.exports = function(){
         // {id : 3, timestamp : Date.now(), started : false , users : []},
     ]
     
-    factory.addGame = function(){
+    factory.addGame = function(game){
         factory.games.push({ timestamp : Date.now(), started : false , users : []})
         console.log(factory.games);
     }
     
     factory.joinGame = function(game){
              game.users.push("Bram");
+    }
+    
+    
+    factory.getGames = function(games){
+       games.forEach(function(game){
+        factory.games.push(game);
+       })
+       console.log(factory.games)
+
     }
     
     
